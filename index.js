eadf1fa65038cd1609d551ba00b96ffd6bad5d01
Michael <mikahado@gmail.com>
2022-10-14 19:15:10 -0400

    commit tree 25f00156202a9259ee381d66b87f4d51c400c046

--- index.js
+++ index.js
@@ -1,1 +1,160 @@
-// Your code here
+
+
+
+// * **Argument(s)**
+//   * A 4-element Array of a `String`, `String`, `String`, and `Number`
+//     corresponding to a first name, family name, title, and pay rate per hour
+// * **Returns**
+//   * JavaScript `Object` with keys:
+//     * `firstName`
+//     * `familyName`
+//     * `title`
+//     * `payPerHour`
+//     * `timeInEvents`
+//     * `timeOutEvents`
+// * **Behavior**
+//   * Loads `Array` elements into corresponding `Object` properties.
+//     _Additionally_, initialize empty `Array`s on the properties `timeInEvents`
+//     and `timeOutEvents`.
+
+
+function createEmployeeRecord(employee) {
+return {
+
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
+    }
+}
+// ### `createEmployeeRecords`
+
+// * **Argument(s)**
+//   * `Array` of `Arrays`
+// * **Returns**
+//   * `Array` of `Object`s
+// * **Behavior**
+//   * Converts each nested `Array` into an employee record using
+//     `createEmployeeRecord` and accumulates it to a new `Array`
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+// ### `createTimeInEvent`
+
+// * **Argument(s)**
+//   * An employee record `Object`
+//   * A date stamp (`"YYYY-MM-DD HHMM"`)
+// * **Returns**
+//   * The employee record
+// * **Behavior**
+//   * Add an `Object` with keys to the `timeInEvents` `Array` on the record
+//     `Object`:
+//     * `type`: Set to `"TimeIn"`
+//     * `hour`: Derived from the argument
+//     * `date`: Derived from the argument
+
+function createTimeInEvent(event) {
+
+    let [date, hour] = event.split(" ")
+
+
+    let eventObj = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    }
+
+    this.timeInEvents.push(eventObj)
+
+    return this
+
+}
+
+function createTimeOutEvent(event) {
+
+    let [date, hour] = event.split(" ")
+
+
+    let eventObj = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    }
+
+
+    employee.timeOutEvents.push(eventObj)
+    
+    return employee
+
+}
+
+ 
+
+
+
+
+
+
+
+
+// ### `createTimeOutEvent`
+
+// * **Argument(s)**
+//   * An employee record `Object`
+//   * A date stamp (`"YYYY-MM-DD HHMM"`)
+// * **Returns**
+//   * The employee record
+// * **Behavior**
+//   * Add an `Object` with keys to the `timeOutEvents` `Array` on the record
+//     `Object`:
+//     * `type`: Set to `"TimeOut"`
+//     * `hour`: Derived from the argument
+//     * `date`: Derived from the argument
+
+// ### `hoursWorkedOnDate`
+
+// * **Argument(s)**
+//   * An employee record `Object`
+//   * A date of the form `"YYYY-MM-DD"`
+// * **Returns**
+//   * Hours worked, an `Integer`
+// * **Behavior**
+//   * Given a date, find the number of hours elapsed between that date's
+//     timeInEvent and timeOutEvent
+
+// ### `wagesEarnedOnDate`
+
+// * **Argument(s)**
+//   * An employee record `Object`
+//   * A date of the form `"YYYY-MM-DD"`
+// * **Returns**
+//   * Pay owed
+// * **Behavior**
+//   * Using `hoursWorkedOnDate`, multiply the hours by the record's
+//     payRate to determine amount owed. Amount should be returned as a number.
+
+// ### `allWagesFor`
+
+// * **Argument(s)**
+//   * An employee record `Object`
+// * **Returns**
+//   * Pay owed for all dates
+// * **Behavior**
+//   * Using `wagesEarnedOnDate`, accumulate the value of all dates worked by the
+//     employee in the record used as context. Amount should be returned as a
+//     number. **HINT**: You will need to find the available dates somehow...
+
+// ### `calculatePayroll`
+
+// * **Argument(s)**
+//   * `Array` of employee records
+// * **Returns**
+//   * Sum of pay owed to all employees for all dates, as a number
+// * **Behavior**
+//   * Using `wagesEarnedOnDate`, accumulate the value of all dates worked by the
+//     employee in the record used as context. Amount should be returned as a
+//     number.
